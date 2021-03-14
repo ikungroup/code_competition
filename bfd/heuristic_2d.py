5d50b3bec0011b8e3e787c9c5c5207565d934339
Lxing_Wang <49400003+WalterLxing@users.noreply.github.com>
2021-03-14 18:56:36 +0800

    二维装箱问题FFD实现，物品排序使用各维度加权

--- bfd/heuristic_2d.py
+++ bfd/heuristic_2d.py
@@ -1,17 +1,24 @@
 '''
-二维装箱问题
+使用FFD(First Fit Decreasing)解决二维装箱问题,箱子容量两个维度，箱子种类只有一种
+bin_cls: 箱子种类，这里只有一种 [种类编号，CPU，内存]
+items: 待装箱的物品 [[物品编号，CPU，内存]，...，[...]]
 '''
 
 
-#箱子容量两个维度，箱子种类只有一种的装箱问题
+
 import numpy as np
 bin_cls = np.array([1,32,128])
-items = np.array([[1,8,12],[2,4,12],[3,32,42]])
-bin_cpu_cap = 32
-bin_mem_cap = 128
+items = np.array([[1,8,12],[2,4,12],[3,32,42],[4,24,32]])
+bin_cpu_cap = 32   #CPU维度的最大容量
+bin_mem_cap = 128  #内存维度的最大容量
 
 
 def sort_items(items):
+    '''
+    对物品进行排序，以物品在维度上的容量比上总容量作为重要性，最后对各个维度相加
+    :param items: 待装箱的物品 [[物品编号，CPU，内存]，...，[...]]
+    :return: 按照重要性程度排序后的物品 [[物品编号，CPU，内存]，...，[...]]
+    '''
     scores = items.astype(np.float64)
     belta_1 = 1/sum(items[:,1:])    #用于评价物品各个维度重要性的参数
     scores[:,1:] = belta_1 * items[:,1:]
@@ -23,48 +30,42 @@ def sort_items(items):
     items = np.delete(items,0,axis=1)
 
     return items
-def sort_bins(bins):
-    scores = bins.astype(np.float64)
-    belta_1 = 1 / sum(bins[:, 1:])  # 用于评价物品各个维度重要性的参数
-    scores[:, 1:] = belta_1 * bins[:, 1:]
-    scores[:, 1:] = np.sum(scores[:, 1:], axis=1, keepdims=1)
-    scores = scores[:, :2]
-    # scores = scores[scores[:,1].argsort()[::-1]]
-    items = np.insert(bins.astype(np.float64), 0, np.array(scores[:, 1]), axis=1)
-    items = items[items[:, 0].argsort()[::-1]]
-    items = np.delete(items, 0, axis=1)
 
-    return bins
-#Best Bin Decearsing methods
-def bfd(bin_cls,items):
+# def sort_bins(bins):
+#     '''
+#       对箱子进行排序，以箱子在维度上的容量比上总容量作为重要性，最后对各个维度相加
+#     :param bins:  箱子 [[箱子种类编号，CPU，内存]，...，[...]]
+#     :return:  按照重要性程度排序后的箱子 [[箱子编号，CPU，内存]，...，[...]]
+#     '''
+#     scores = bins.astype(np.float64)
+#     belta_1 = 1 / sum(bins[:, 1:])  # 用于评价物品各个维度重要性的参数
+#     scores[:, 1:] = belta_1 * bins[:, 1:]
+#     scores[:, 1:] = np.sum(scores[:, 1:], axis=1, keepdims=1)
+#     scores = scores[:, :2]
+#     # scores = scores[scores[:,1].argsort()[::-1]]
+#     items = np.insert(bins.astype(np.float64), 0, np.array(scores[:, 1]), axis=1)
+#     items = items[items[:, 0].argsort()[::-1]]
+#     items = np.delete(items, 0, axis=1)
+#
+#     return bins
 
-    failure = 0
+
+#First Fit Decearsing methods
+def ffd(bin_cls,items):
+
+
     items = sort_items(items) #sort the items
     if len(items) > 0:
         bins = np.array([bin_cls])
-    bins = sort_bins(bins)
+    # bins = sort_bins(bins)  因为只有一种箱子，所以不需要排序
     for item_num in range(len(items)):
         # for bins_num in range(len(bins)):
         bins = pack_item(bins,items[item_num])
+    return bins
 
 
 
-    #     bf_num, bf_cap_remain = bf(bins,items[item_num])
-    #     # 如果当前箱子内装不下物品但是全空的箱子可以，创建新的空箱子
-    #     if (bf_num == -1) and (items[item_num] <=bin_cap):
-    #         bins.append(bin_cap - items[item_num])
-    #         continue
-    #     elif (bf_num == -1): #如果没有箱子可以装下
-    #         failure = 1
-    #         break
-    #     else: #如果可以装下，更新箱子容量
-    #         bins[bf_num] = bf_cap_remain
-    # if failure == 1:
-    #     raise Exception('物体体积{}大于箱子容量{}'.format(items[item_num],bin_cap))
-    #
-    # bins_res = np.array(bins)  #箱子剩余容量
-    # bins_used = bin_cap - np.array(bins)
-    # return bins_res,bins_used
+
 
 def pack_item(bins,item):
     '''
@@ -89,36 +90,11 @@ def pack_item(bins,item):
         return bins
 
 
-bfd(bin_cls,items)
 
 
 
 
+if __name__ == '__main__':
+    bins=ffd(bin_cls,items)
+    a = 2
 
-#找到装物品之后使得剩余容量最小的箱子
-def bf(bin_cap,item):
-    bf_cap_remain = 1e10
-    bf_num = -1
-    for i in range(len(bin_cap)):
-        if ((bin_cap[i] - item) >= 0):
-            cap_remain = bin_cap[i] - item
-            if(cap_remain<bf_cap_remain):
-                bf_cap_remain = cap_remain
-                bf_num = i
-    return bf_num,bf_cap_remain
-
-bin_cap = [10,6,4,5,3]
-bf_num,bf_cap_remain = bf(bin_cap,11)
-o=2
-
-
-
-
-
-a= sort_items(items)
-
-print(a)
-
-bins_res,bins_used =  bfd(bin,items)
-print('箱子的剩余容量',bins_res)
-print('箱子已经使用容量',bins_used)
