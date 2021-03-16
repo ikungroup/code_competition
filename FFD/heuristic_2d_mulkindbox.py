79e6f6358fbc73d7d1317891e4a426f58c5e98df
lingxing <49400003+lingxing@users.noreply.github.com>
2021-03-16 16:44:22 +0800

    测试测试

--- FFD/heuristic_2d_mulkindbox.py
+++ FFD/heuristic_2d_mulkindbox.py
@@ -9,13 +9,12 @@ ser_used: 服务器的使用列表 [[编号，CPU剩余，内存剩余],...,[...
 from utils.data_load import read_file
 import numpy as np
 import os
-
-np.set_printoptions(suppress=True)  # 不以科学计数法输出
+np.set_printoptions(suppress=True) #不以科学计数法输出
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-bin_cls = np.array([1, 32, 128])
-items = np.array([[1, 8, 12], [2, 4, 12], [3, 32, 42], [4, 24, 32]])
-bin_cpu_cap = 32  # CPU维度的最大容量
-bin_mem_cap = 128  # 内存维度的最大容量
+bin_cls = np.array([1,32,128])
+items = np.array([[1,8,12],[2,4,12],[3,32,42],[4,24,32]])
+bin_cpu_cap = 32   #CPU维度的最大容量
+bin_mem_cap = 128  #内存维度的最大容量
 
 
 def sort_items(items):
@@ -25,17 +24,16 @@ def sort_items(items):
     :return: 按照重要性程度排序后的物品 [[物品编号，CPU，内存]，...，[...]]
     '''
     scores = items.astype(np.float64)
-    belta_1 = 1 / sum(items[:, 1:])  # 用于评价物品各个维度重要性的参数
-    scores[:, 1:] = belta_1 * items[:, 1:]
-    scores[:, 1:] = np.sum(scores[:, 1:], axis=1, keepdims=1)
-    scores = scores[:, :2]
+    belta_1 = 1/sum(items[:,1:])    #用于评价物品各个维度重要性的参数
+    scores[:,1:] = belta_1 * items[:,1:]
+    scores[:,1:] = np.sum(scores[:,1:],axis=1,keepdims=1)
+    scores = scores[:,:2]
     # scores = scores[scores[:,1].argsort()[::-1]]
-    items = np.insert(items.astype(np.float64), 0, np.array(scores[:, 1]), axis=1)
-    items = items[items[:, 0].argsort()[::-1]]
-    items = np.delete(items, 0, axis=1)
+    items = np.insert(items.astype(np.float64),0,np.array(scores[:,1]),axis = 1)
+    items = items[items[:,0].argsort()[::-1]]
+    items = np.delete(items,0,axis=1)
 
     return items
-
 
 def sort_bins(bins):
     '''
@@ -56,42 +54,47 @@ def sort_bins(bins):
     return bins
 
 
-def find_first_vir(vir_need, mode='max'):
+def find_first_vir(vir_need,mode='max'):
     if mode == 'max':
         return vir_need[0]
 
 
-def fit_sever(sever_cls, vir, cpu_lab=1, mem_lab=2):
+def fit_sever(sever_cls,vir,cpu_lab=1,mem_lab=2):
     '''
     找到服务器列表中能够装下虚拟机的返回
     :param sever_cls: 服务器列表 [[编号，CPU，内存],...,[...]]
     :param vir: 虚拟机[编号，CPU，内存]
     :return: 返回能够装下服务器的列表 [[编号，CPU，内存],...,[...]]
     '''
-    new_sever_cls = [[0, 0, 0]]
+    new_sever_cls = [[0,0,0]]
     for i in range(len(sever_cls)):
         if (sever_cls[i][cpu_lab] >= vir[cpu_lab]) and (sever_cls[i][mem_lab] >= vir[mem_lab]):
-            new_sever_cls = np.append(new_sever_cls, np.array([sever_cls[i]]), axis=0)
-    new_sever_cls = np.delete(new_sever_cls, 0, axis=0)
+            new_sever_cls = np.append(new_sever_cls,np.array([sever_cls[i]]),axis=0)
+    new_sever_cls = np.delete(new_sever_cls,0,axis=0)
 
     return new_sever_cls
 
 
-def best_fit_sever(sever_cls, vir):
+def best_fit_sever(sever_cls,vir):
     '''
     在服务器列表中找到最适合虚拟机的服务器进行分配
     :param sever_cls: 服务器种类列表或者使用的服务器列表 [[编号，CPU，内存],...,[...]]
     :param vir: 需要分配的虚拟机[编号，CPU，内存]
     :return sever: 最合适的服务器 [编号，CPU，内存]
     '''
-    sever_cls = fit_sever(sever_cls, vir)  # 首先找到能够容下服务器的列表
-    scores = sever_cls
-    scores[:, 1:] = np.divide(vir[1:], sever_cls[:, 1:])
 
+    scores = sever_cls.copy()
+    scores[:,1:] = np.divide(vir[1:],scores[:,1:])
+    scores[:, 1:] = np.sum(scores[:, 1:], axis=1, keepdims=1)
+    scores = scores[:, 1]
+    sever_cls = np.insert(sever_cls, 0, np.array([scores]), axis=1)
+    sever_cls = sever_cls[sever_cls[:, 0].argsort()[::-1]]
+    sever_cls = np.delete(sever_cls,0,axis=1)
+    sever = sever_cls[0,:]
     return sever
 
 
-def assign(vir, sever, cpu_lab=1, mem_lab=2):
+def assign(vir,sever,sever_used,in_use=False,first_alloc=False):
     '''
     对服务器分配CPU，内存，返回分配后服务器参数
     :param vir: 虚拟机 [编号，CPU，内存]
@@ -100,63 +103,82 @@ def assign(vir, sever, cpu_lab=1, mem_lab=2):
     :param end: 结束分配的序号
     :return: 分配后的服务器
     '''
+    sever[1] = sever[1] - vir[1]
+    sever[2] = sever[2] - vir[2]
+    if first_alloc == True:
+        sever_used = np.array([sever])
+        return sever_used
 
-    sever[cpu_lab] = sever[cpu_lab] - vir[cpu_lab]
-    sever[mem_lab] = sever[mem_lab] - vir[mem_lab]
+    if in_use == False: #如果分配的服务器目前不在使用
+        sever_used = np.append(sever_used,np.array([sever]),axis=0)
+    else:
+        index = np.squeeze(np.where(sever_used[:,0] == sever[0]))
+        sever_used[index,1] = sever[1]
+        sever_used[index, 2] = sever[2]
 
-    return sever
+    return sever_used
 
 
-def first_assign(sever_cls, vir_need):
+def first_assign(sever_cls,vir_need):
     vir = find_first_vir(vir_need)
     vir = vir_need[0]
-    sever = best_fit_sever(sever_cls, vir)
-    sever = assign(vir, sever)
-    sever_used = np.array([[sever]])
+    sever_cls = fit_sever(sever_cls, vir)  # 首先找到能够容下服务器的列表
+    sever = best_fit_sever(sever_cls,vir)
+    sever_used = assign(vir, sever,sever_used=[[]],in_use=False,first_alloc=True)
+    # sever[1] = sever[1] - vir[1]
+    # sever[2] = sever[2] - vir[2]
+    # sever_used = np.array([sever])
+    vir_need = np.delete(vir_need,0,axis=0)
 
-    return sever_used, vir_need
+    return sever_used,vir_need
 
 
-def heuristic(vir_need, sever_cls, ):
-    # 首先对虚拟机进行排序
+def heuristic(vir_need,sever_cls):
+    #首先对虚拟机进行排序
     vir_need = sort_items(vir_need)
-    sever_used, vir_need = first_assign(sever_cls, vir_need)
+    sever_used,vir_need = first_assign(sever_cls,vir_need)
+
+    for i in range(len(vir_need)):
+        fit_sever_cls = fit_sever(sever_used,vir_need[i])
+        if len(fit_sever_cls) == 0: #已经使用服务器中没有合适容量的
+            fit_sever_cls = fit_sever(sever_cls,vir_need[i])
+            sever = best_fit_sever(fit_sever_cls,vir_need[i])
+            sever_used = assign(vir_need[i],sever,sever_used,in_use=False)
+            # sever_used = np.append(sever_used,np.array([sever]),axis=0)
+        else:#已经使用服务器中有合适的
+            sever = best_fit_sever(fit_sever_cls,vir_need[i])
+            sever_used = assign(vir_need[i], sever,sever_used,in_use=True)
+
+    a = 2
 
 
-def pack_item(bins, item):
-    '''
-    对已经排好序的物品和箱子进行装箱
-    :param bins: 箱子序列，格式为[[序号，物品CPU,物品内存],...,[...]]
-    :param item: 待排序的物品，格式为[序号，物品CPU,物品内存]
-    :return: 装箱结束后的箱子
-    '''
 
-    item_cpu, item_mem = item[1], item[2]
 
-    for bin_num in range(len(bins)):
-        bin_cpu, bin_mem = bins[bin_num][1], bins[bin_num][2]
-        if (item_cpu <= bin_cpu) and (item_mem <= bin_mem):
-            bins[bin_num][1], bins[bin_num][2] = bin_cpu - item_cpu, bin_mem - item_mem
-            return bins
+    return vir_need
 
-    # 原来箱子剩余容量不足以放入物品，加入新的箱子
-    if (item_cpu <= bin_cpu_cap) and (item_mem <= bin_mem_cap):
-        bins = np.insert(bins, 0, np.array([bin_cls]), axis=0)
-        bins[0][1], bins[0][2] = bins[0][1] - item_cpu, bins[0][2] - item_mem
-        return bins
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     data_dir = os.path.join(BASE_DIR, "..", "data", "training-1.txt")
-    vm_cls, sever_cls = read_file(data_dir)
+    sever_cls,vm_cls = read_file(data_dir)
     ONE_DAY = True
     if ONE_DAY:
-        vm_cls = np.delete(vm_cls, 3, axis=1)
-        sever_cls = np.delete(sever_cls, 3, axis=1)
-        sever_cls = np.delete(sever_cls, 3, axis=1)
+        vm_cls = np.delete(vm_cls,3,axis=1)
+        sever_cls = np.delete(sever_cls,3,axis=1)
+        sever_cls = np.delete(sever_cls,3,axis=1)
 
-    # 获取第一天的数据
+    #获取第一天的数据
+
+
 
     vm_cls = sort_items(vm_cls)
     sever_cls = sort_bins(sever_cls)
-    bins = ffd(bin_cls, items)
+    heuristic(vm_cls,sever_cls)
